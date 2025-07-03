3706401b2e4d6d76dcb808be2b1b995a2e0b1323
PrajwalUppoor <36199449+PrajwalUppoor@users.noreply.github.com>
2025-07-04 01:05:20 +0530

    Create models.py

---
+++ models/models.py
@@ -0,0 +1,29 @@
+# models.py
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Swayamsevak(Base):
+    __tablename__ = 'swayamsevaks'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    phone = Column(String)
+    email = Column(String)
+    address1 = Column(String)
+    address2 = Column(String)
+    address3 = Column(String)
+    pincode = Column(String)
+    dob = Column(String)
+    bloodgroup = Column(String)
+    education = Column(String)
+    profession = Column(String)
+    work = Column(String)
+    sanghShikshan = Column(String)
+    sanghaResponsibility = Column(String)
+    sanghOrganizationName = Column(String)
+    otherResponsibility = Column(String)
+    shakhe = Column(String)
+    vasati = Column(String)
+    upavasati = Column(String)
