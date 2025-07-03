bcdf4f1300c829e45ffb92159c2a5da84916c704
PrajwalUppoor <36199449+PrajwalUppoor@users.noreply.github.com>
2025-07-04 01:38:44 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-engine = create_engine("sqlite:///data.db")  # Or use PostgreSQL URI
+DATABASE_URL = "sqlite:///data.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(bind=engine)
 
 def init_db():
